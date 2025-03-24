aef5d4bd3b60b46e206108814d21a96fc0e64799
o1was <guanlinhai520@gmail.com>
2025-03-24 23:40:54 +0800

    ✨ Feat(gorm): gorm
    gorm

--- stores/gormx/scopes/paginate/paginate.go
+++ stores/gormx/scopes/paginate/paginate.go
@@ -15,11 +15,13 @@ func Paginate(pagination *Pagination) func(db *gorm.DB) *gorm.DB {
 		var (
 			tx        = db.Session(&gorm.Session{})
 			totalRows int64
+			offset    = pagination.Offset()
+			limit     = pagination.Limit()
 		)
 
 		tx.Model(db.Statement.Model).Count(&totalRows)
 		pagination.Total = totalRows
 		pagination.TotalPage = int64(math.Ceil(float64(totalRows) / float64(pagination.PageSize)))
-		return db.Offset(int(pagination.Offset())).Limit(int(pagination.Limit()))
+		return db.Offset(int(offset)).Limit(int(limit))
 	}
 }
